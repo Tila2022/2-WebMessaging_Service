8087bcfedca9fd08cc29a2c8196b4b461cb47424
Tila2022 <dtelyuk@mail.ru>
2022-08-10 00:57:03 +0300

    Update README.md

--- src/main/java/MessageRepo.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface MessageRepo extends CrudRepository<Message, Long> {
-    List<Message> findByTag(String tag);
-}
